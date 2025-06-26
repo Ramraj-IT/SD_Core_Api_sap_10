0dbcadd53ecb047fd626c9be686526d1e84cee0d
Sathishkumar.PR.X1 <Sathishkumar.PR.X1@ENES-EDPSW7>
2025-06-26 12:10:19 +0530

    Hangfire added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Hangfire;
+using Hangfire.Dashboard.BasicAuthorization;
+using Hangfire.MemoryStorage;
+using Hangfire.SqlServer;
 using SD_Core_Api.Interfaces;
+using SD_Core_Api.Jobs;
 using SD_Core_Api.Repository;
 using SD_Core_Api.Services;
 
@@ -11,25 +16,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddHangfire(configuration => configuration
-//.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-//.UseSimpleAssemblyNameTypeSerializer()
-//.UseRecommendedSerializerSettings()
-//.UseMemoryStorage()
-//.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireDBConnection"), new SqlServerStorageOptions
-//{
-//    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-//    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-//    QueuePollInterval = TimeSpan.Zero,
-//    UseRecommendedIsolationLevel = true,
-//    DisableGlobalLocks = true
-//})
-//);
+builder.Services.AddHangfire(configuration => configuration
+.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+.UseSimpleAssemblyNameTypeSerializer()
+.UseRecommendedSerializerSettings()
+.UseMemoryStorage()
+.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireDBConnection"), new SqlServerStorageOptions
+{
+    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+    QueuePollInterval = TimeSpan.Zero,
+    UseRecommendedIsolationLevel = true,
+    DisableGlobalLocks = true
+})
+);
 
-//var options = new BackgroundJobServerOptions()
-//{
-//    Queues = new[] { "queue-a_ap", "queue-b_dn", "queue-c_cn", "queue-d_ocr", "queue-e_nrdc", "queue-f_grn", "queue-g_stk", "queue-h_bp", "queue-i_jrnl" }
-//};
+var options = new BackgroundJobServerOptions()
+{
+    Queues = new[] { "queue-a_ap", "queue-b_dn", "queue-c_cn", "queue-d_ocr", "queue-e_nrdc", "queue-f_grn", "queue-g_stk", "queue-h_bp", "queue-i_jrnl" }
+};
 
 
 
@@ -55,42 +60,42 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//new BackgroundJobServer(options);
+new BackgroundJobServer(options);
 
-//app.UseHangfireDashboard("/hangfire", new DashboardOptions
-//{
-//    Authorization = new[] { new BasicAuthAuthorizationFilter(
-//                    new BasicAuthAuthorizationFilterOptions
-//                    {
-//                        RequireSsl = false,
-//                        SslRedirect = false,
-//                        LoginCaseSensitive = true,
-//                        Users = new[]
-//                        {
-//                            new BasicAuthAuthorizationUser
-//                            {
-//                                Login = "Admin",
-//                                PasswordClear = "Admin"
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    Authorization = new[] { new BasicAuthAuthorizationFilter(
+                    new BasicAuthAuthorizationFilterOptions
+                    {
+                        RequireSsl = false,
+                        SslRedirect = false,
+                        LoginCaseSensitive = true,
+                        Users = new[]
+                        {
+                            new BasicAuthAuthorizationUser
+                            {
+                                Login = "Admin",
+                                PasswordClear = "123456"
 
-//                            }
-//                        }
-//                    }) },
-//    AppPath = "/hangfire",
-//    DefaultRecordsPerPage = 20,
-//    DisplayStorageConnectionString = true,
-//    DarkModeEnabled = true,
-//    DashboardTitle = "Tasks Dashboard"
-//});
+                            }
+                        }
+                    }) },
+    AppPath = "/hangfire",
+    DefaultRecordsPerPage = 20,
+    DisplayStorageConnectionString = true,
+    DarkModeEnabled = true,
+    DashboardTitle = "Tasks Dashboard"
+});
 
 
-//app.UseHangfireDashboard("/hangfire");
+app.UseHangfireDashboard("/hangfire");
 
-//app.UseHangfireDashboard("/hangfire", new DashboardOptions
-//{
-//    Authorization = new[] {new Hangfire.Dashboard.AllowAllConnectionsFilter()} 
-//});
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    Authorization = new[] { new Hangfire.Dashboard.AllowAllConnectionsFilter() }
+});
 
-//RecurringJob.AddOrUpdate<SAPtoSD>("SAP_SD_Post_AP", x => x.SAPSDAP(), "0/30 * * * *");
+RecurringJob.AddOrUpdate<SAPtoSD>("SAP_SD_Post_AP", x => x.SAPSDAP(), "0/30 * * * *");
 //RecurringJob.AddOrUpdate<SAPtoSD>("SAP_SD_Post_DN", x => x.SAPSDDN(), "0/30 * * * *");
 //RecurringJob.AddOrUpdate<SAPtoSD>("SAP_SD_Post_CN", x => x.SAPSDCN(), "0/30 * * * *");
 //RecurringJob.AddOrUpdate<SAPtoSD>("SAP_SD_Post_CR", x => x.SAPSDOCR(), "0/30 * * * *");
